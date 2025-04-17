d7a3c1b4f1bbe0a2f629df06279528c174719f96
naddicott-dtech <59628939+naddicott-dtech@users.noreply.github.com>
2025-04-17 11:32:11 -0700

    Update stepController.js
    for more clicking...

--- js/stepController.js
+++ js/stepController.js
@@ -1,19 +1,35 @@
-// js/stepController.js
+// stepController.js
 
 import { selectedTools } from './toolSystem.js';
 
-export function isToolComboValid(validCombos) {
-	// Checks if selectedTools matches any allowed combo, order doesn't matter
+/**
+ * Returns true if the currently selected tools match any of the valid tool combinations.
+ * @param {Array<Array<string>>} validCombos
+ * @returns {boolean}
+ */
+export function comboIsValid(validCombos) {
 	return validCombos.some(
-		combo => combo.length === selectedTools.length &&
-			combo.every(t => selectedTools.includes(t))
+		(valid) =>
+			selectedTools.length === valid.length &&
+			valid.every(t => selectedTools.includes(t))
 	);
 }
 
-export function handlePartInteraction(partName, step, onSuccess, onFail) {
-	if (isToolComboValid(step.valid_tool_combinations)) {
-		onSuccess(partName, step);
-	} else {
-		onFail();
-	}
+/**
+ * Attaches a click handler to a model part element.
+ * When clicked, runs onSuccess if tool combo is valid, otherwise onFail.
+ * @param {HTMLElement} partEntity
+ * @param {Object} step
+ * @param {Function} onSuccess
+ * @param {Function} onFail
+ */
+export function attachPartClickHandler(partEntity, step, onSuccess, onFail) {
+	partEntity.classList.add("clickable");
+	partEntity.addEventListener("click", () => {
+		if (comboIsValid(step.valid_tool_combinations)) {
+			onSuccess(partEntity, step);
+		} else {
+			onFail(partEntity, step);
+		}
+	});
 }
